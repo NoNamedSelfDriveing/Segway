29d4f36779552d416f18191d5a0078d9d0abb41c
Min Seok Kim <buildorder@naver.com>
2016-10-16 01:30:27 +0900

    세움지기 라이브러리 CPP V3
    헤더파일 참조

--- Seum.cpp
+++ Seum.cpp
@@ -29,7 +29,12 @@ void Seum::IMUSetup()
 {
   Wire.begin();
   Serial.begin(115200);
-
+/*
+  Wire.beginTransmission(MPU6050);
+  Wire.write(0x19);
+  Wire.write(7);
+  Wire.endTransmission(true);
+*/
   Wire.beginTransmission(MPU6050);
   Wire.write(0x6B);
   Wire.write(0);
@@ -56,10 +61,10 @@ void Seum::GetIMUData(int *IMUDataArray)
 
 double Seum::ComplementaryFilter(int *IMUDataArray)
 {
-    _accXangle = atan2(IMUDataArray[1], IMUDataArray[0]) * RAD_TO_DEG;
+    _accXangle = (atan2(IMUDataArray[0], IMUDataArray[1]) + PI) * RAD_TO_DEG;
     _gyroXangle = (double)IMUDataArray[2] / 131.0;
 
-    _currentAngle = (  0.93 * (_currentAngle + (_gyroXangle * (double)GetMicroTimeGap() / 1000000) )  + ( 0.07 * _accXangle) );
+    _currentAngle = (  0.98 * (_currentAngle + (_gyroXangle * (double)GetMicroTimeGap() / 1000000) )  + ( 0.02 * _accXangle) );
 
     return _currentAngle;
 }
@@ -69,15 +74,15 @@ double Seum::KalmanFilter(int *IMUDataArray)
 
 }
 
-int Seum::GetControlValue(int receiveAngle)
+int Seum::GetControlValue(double receiveAngle)
 {
   _currentError = TARGET - receiveAngle;
-  _pControl = _currentError * Kp;
+  _pControl = _currentError * _Kp;
 
-  _integralError += _currentError * (double)GetMicroTimeGap() / 1000000;
-  _iControl = _integralError * Ki;
+  _integralError += _currentError * _timeGap;
+  _iControl = _integralError * _Ki;
 
-  _dControl = ( _currentError - _prevError ) * Kd;
+  _dControl = ( _currentError - _prevError ) * _Kd;
 
   _prevError = _currentError;
 
@@ -85,6 +90,29 @@ int Seum::GetControlValue(int receiveAngle)
   _control = constrain(_control, -MAX_PWM, MAX_PWM);
 
   return _control;
+}
+
+int Seum::GetControlValue(double receiveAngle, double *ControlDataArr)
+{
+  _currentError = TARGET - receiveAngle;
+  _pControl = _currentError * _Kp;
+  ControlDataArr[0] = _pControl;
+
+  _integralError += _currentError * _timeGap / 1000000;
+  _integralError = constrain(_integralError, -15, 15);
+  _iControl = _integralError * _Ki;
+
+  ControlDataArr[1] = _iControl;
+
+  _dControl = ( _currentError - _prevError ) * _Kd;
+  ControlDataArr[2] = _dControl;
+
+  _prevError = _currentError;
+
+  _control = _pControl + _iControl + _dControl;
+  //_control = constrain(_control, -MAX_PWM, MAX_PWM);
+
+  return constrain(_control * _K, -MAX_PWM, MAX_PWM);
 }
 
 void Seum::MotorControl(int receiveControlValue)
@@ -95,10 +123,26 @@ void Seum::MotorControl(int receiveControlValue)
   digitalWrite(_RightMotorRed, receiveControlValue > 0 ? LOW : HIGH);
 
   if (receiveControlValue < 0)
-    _control = map(receiveControlValue, 0, -255, 0, 255);
+    receiveControlValue = map(receiveControlValue, 0, -255, 0, 255);
 
   analogWrite(_PWMA, receiveControlValue);
   analogWrite(_PWMB, receiveControlValue);
+}
+
+void Seum::GetPIDGainData(double *PIDGainArray)
+{
+  PIDGainArray[0] = ( analogRead(0) / 1023.0 ) * 10.0;
+  PIDGainArray[1] = ( analogRead(1) / 1023.0 ) * 10.0;
+  PIDGainArray[2] = ( analogRead(2) / 1023.0 ) * 10.0;
+  PIDGainArray[3] = ( analogRead(3) / 1023.0 ) * 10.0;
+}
+
+void Seum::SetPIDGain(double *PIDGainArray)
+{
+  _Kp = PIDGainArray[0];
+  _Ki = PIDGainArray[1];
+  _Kd = PIDGainArray[2];
+  _K  = PIDGainArray[3];
 }
 
 unsigned long Seum::GetMicroTimeGap()
@@ -107,4 +151,26 @@ unsigned long Seum::GetMicroTimeGap()
   _currentTime = micros();
 
   return _timeGap;
+}
+
+void Seum::PrintAll()
+{
+  Serial.print("TIME : ");
+  Serial.print(_timeGap / 1000);
+  Serial.print("    ");
+
+  Serial.print("Kp : ");
+  Serial.print(_Kp);
+  Serial.print("    ");
+
+  Serial.print("Ki : ");
+  Serial.print(_Ki);
+  Serial.print("    ");
+
+  Serial.print("Kd : ");
+  Serial.print(_Kd);
+  Serial.print("    ");
+
+  Serial.print("K : ");
+  Serial.println(_K);
 }
