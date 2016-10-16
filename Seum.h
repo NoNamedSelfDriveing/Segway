91b595b00347f2af6130b0348a7f8a520582d660
Min Seok Kim <buildorder@naver.com>
2016-10-16 22:37:55 +0900

    세움지기 라이브러리 헤더파일 V4
    1. 단계별로 라이브러리 몇개 추가 ( 아마 이게 완성일듯 싶음)
    
    2. 주석 추가

--- Seum.h
+++ Seum.h
@@ -1,19 +1,9 @@
 #ifndef Seum_h
 #define Seum_h
 
-#ifndef Kalman_h
-#define Kalman_h
-#include <Kalman.h>
-#endif
-
 #ifndef Wire_h
 #define Wire_h
 #include <Wire.h>
-#endif
-
-#ifndef I2Cdev_h
-#define I2Cdev_h
-#include <I2Cdev.h>
 #endif
 
 #ifndef MPU6050
@@ -23,7 +13,7 @@
 #define TARGET 180
 #define MAX_PWM 255
 
-#define runEvery(t) for (static typeof(t) _lasttime;(typeof(t))((typeof(t))millis() - _lasttime) > (t);_lasttime += (t))
+#define runEvery(t) for (static typeof(t) _lasttime; (typeof(t)) ((typeof(t))millis() - _lasttime) > (t); _lasttime += (t))
 
 #include "Arduino.h"
 
@@ -31,26 +21,43 @@ class Seum
 {
   public:
 
-    Seum();
-    Seum(int PWMA, int PWMB, int LMB, int LMR, int RMB, int RMR);
+    /* 생성자 */
+    Seum();   // 기본적으로 세팅
+    Seum(int PWMA, int PWMB, int LMB, int LMR, int RMB, int RMR);   // 자신이 원하는 대로 핀 세팅
+    /* 생성자 */
 
-    void IMUSetup();
-    void GetIMUData(int IMUDataArray[]);
-    void GetPIDGainData(double PIDGainArray[]);
-    void SetPIDGain(double PIDGainArray[]);
-    double ComplementaryFilter(int IMUDataArray[]);
-    double KalmanFilter(int IMUDataArray[]);
-    unsigned long GetMicroTimeGap();
+    /* 기울기 값 구하기 */
+    void IMUSetup();    // IMU 켜줌
+    void GetIMURawData(int IMUDataArray[]);   // 기울기 센서의 필터가 적용 되지 않은 값 구하기 가속도 센서의 Y, Z 값과 자이로 센서의 X 값만
+    double ComplementaryFilter(int IMUDataArray[]);   // 가공되지 않은 값들을 상보필터를 통해서 기울기로 얻는다.
+    double GetGradient();   // GetIMURawData함수와 ComplementaryFilter함수를 한곳에 모아논 함수
+    /* 기울기 값 구하기 */
 
-    int GetControlValue(double receiveAngle);
-    int GetControlValue(double receiveAngle, double ControlDataArr[]);
-    void MotorControl(int receiveControlValue);
-    void PrintAll();
+    /* 가변저항 PID 게인 값 구하기 & 세팅 */
+    void GetPIDGainData(double PIDGainArray[]);   // 가변저항 0~3 부터 읽기
+    void SetPIDGain(double PIDGainArray[]);   // 해당 버퍼 순서대로 / 0 : Kp  / 1 : Ki  / 2: Kd  / 3: K /으로세팅
+    /* 가변저항 PID 게인 값 구하기 & 세팅 */
 
+    /* PID 계산과, 모터 움직임 */
+    int GetControlValue(double receiveAngle);   // PID 계산을 통해 control 값 구하기
+    int GetControlValue(double receiveAngle, double ControlDataArr[]);    // 위와 하는 역할은 같지만, ControlDataArr버퍼에 PID 각각의 Control 값 담기
+    void MotorControl(int receiveControlValue);   // PID 계산을 통해 얻은 Control 값으로 모터 제어
+    void PIDMotor(double receiveAngle);   //   GetControlValue 함수와 MotorControl 함수를 한곳에 모아논 함수
+    /* PID 계산과, 모터 움직임 */
+
+    /* 잡기능 */
+    unsigned long GetMicroTimeGap();  //한 루프가 도는 속도 Micro단위로 구함 dt(delta time)구하는데 사용
+    void PrintAll(); // 현재 여러가지 상태들 출력
+    /* 잡기능 */
+
+    /* 단계별 함수들 */
+    void LevelOne();
+    void LevelTwo();
+    /* 단계별 함수들 */
 
 
   private:
-    /* PINS */
+    /* 핀들 */
     int _PWMA;
     int _PWMB;
 
@@ -58,39 +65,46 @@ class Seum
     int _LeftMotorRed;
     int _RightMotorBlack;
     int _RightMotorRed;
-    /* PINS */
+    /* 핀들 */
 
     /* PID */
-    double _currentError;
-    double _prevError;
-    double _integralError;
+    double _currentError;   // 현재 에러
+    double _prevError;    // 이전의 에러
+    double _integralError;    // 적분된(누적된) 에러
 
-    double _pControl;
-    double _iControl;
-    double _dControl;
+    double _pControl;   // P 제어 값
+    double _iControl;   // I 제어 값
+    double _dControl;   // D 제어 값
 
-    int _control;
+    int _control;   // PID 제어 값
 
-    double _Kp;
-    double _Ki;
-    double _Kd;
-    double _K;
+    double _Kp;   // P 게인 값
+    double _Ki;   // I 게인 값
+    double _Kd;   // D 게인 값
+    double _K;    // K 게인 값
     /* PID */
 
-    /* IMU */
+    /* 기울기 센서 */
     int _accX, _accY, _accZ;
     int _temperature;
     int _gyroX, _gyroY, _gyroZ;
 
-    double _accXangle;
-    double _gyroXangle;
+    double _accXangle;    // atan2를 통해 나온 가속도 기울기 값
+    double _gyroXangle;   // 자이로센서로 구한 기울기 값
 
-    double _currentAngle;
-    /* IMU */
+    double _currentAngle;  // 상보필터를 통해 구한 기울기 값
+    /* 기울기 센서 */
 
+    /* BUFFERS */
+    int _IMUDataArray[3];
+    /* BUFFERS */
+
+    /* TIMER */
     unsigned long _currentTime = 0;
     unsigned long _timeGap;
-
+    unsigned long _millisCurrentTime = 0;
+    unsigned long _millisTimeGap;
+    /* TIMER */
 };
 
 #endif
